ac9b0e9e7591063f1619cfcd2d6ae56d68d3913c
Bin Ren <renbin@outlook.com>
2017-11-23 18:50:28 +0800

    message

--- datavis/test.py
+++ datavis/test.py
@@ -1,10 +1,11 @@
 import numpy as np
-from matplotlib.pyplot import plot, show
-x = np.linspace(0, 2 * np.pi, 30)
-wave = np.cos(x)  #创建一个包含30个点的余弦波信号。
-transformed = np.fft.fft(wave)  #使用fft函数对余弦波信号进行傅里叶变换。
-shifted = np.fft.fftshift(transformed) #使用fftshift函数进行移频操作。
-print(np.all((np.fft.ifftshift(shifted) - transformed) < 10 ** -9))  #用ifftshift函数进行逆操作，这将还原移频操作前的信号。
-plot(transformed, lw=2)
-plot(shifted, lw=3)
-show()
+import pandas as pd
+
+if __name__=="__main__":
+    for i in range(0,3):
+        print(i)
+    obj = pd.Series(range(3),index=['a','b','c'])
+    print(obj)
+
+    frame = pd.DataFrame(np.arange(9).reshape(3,3),index=['a','b','c'],columns=['ac','bc','cc'])
+    print(frame)
---
+++ mutiprocClass.py
@@ -0,0 +1,21 @@
+import multiprocessing
+import time
+
+class ClockProcess(multiprocessing.Process):
+    def __init__(self, interval):
+        multiprocessing.Process.__init__(self)
+        self.interval = interval
+
+    def run(self):
+        n = 5
+        while n > 0:
+            print("the time is {0}".format(time.ctime()))
+            time.sleep(self.interval)
+            n -= 1
+
+if __name__ == '__main__':
+    p = ClockProcess(3)
+    p.daemon = True  #因子进程设置了daemon属性，主进程结束，它们就随着结束了。
+    p.start()
+    time.sleep(10)
+    print("in main")
